b41fabfcd473b8b237b0b3c29f5b5a210595dd02
SUBHA PANDIT <subhapandit@SUBHAs-MacBook-Air.local>
2026-09-02 12:34:01 +0530

    Create env file for Xcode Cloud

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
-
 set -e
 
 cd "$CI_PRIMARY_REPOSITORY_PATH"
+
+echo "Creating .env from Xcode Cloud environment variables..."
+
+printf '%s\n' \
+  "GOOGLE_MAPS_API_KEY=$GOOGLE_MAPS_API_KEY" \
+  "MSG91_WIDGET_ID=$MSG91_WIDGET_ID" \
+  "MSG91_AUTH_TOKEN=$MSG91_AUTH_TOKEN" \
+  "SUPABASE_URL=$SUPABASE_URL" \
+  "SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY" \
+  > .env
+
+echo ".env created successfully."
 
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$HOME/flutter"
 
 export PATH="$PATH:$HOME/flutter/bin"
 
 flutter precache --ios
-
 flutter pub get
 
 HOMEBREW_NO_AUTO_UPDATE=1 brew install cocoapods
 
 cd ios
 pod install
-
-exit 0
